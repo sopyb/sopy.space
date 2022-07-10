328e4fc86cc213c6bb09a567bf4b32be9de137be
sopy <doimpt@sopy.one>
2022-07-10 13:38:11 +0300

    Mobile parallax?

--- src/js/starHandler.js
+++ src/js/starHandler.js
@@ -28,7 +28,7 @@ $(document).ready(() => {
     }
 })
 
-//parallax
+//parallax desktop
 $(document).mousemove((e) => {
     let x = e.pageX,
         y = e.pageY,
@@ -37,7 +37,21 @@ $(document).mousemove((e) => {
         xpr = x/width*100-50,
         ypr = y/height*100-50
 
-    containers[0].css({"bottom": ((ypr/30) + "%"), "right": ((xpr / 30) + "%"), })
-    containers[1].css({"bottom": ((ypr/25) + "%"), "right": ((xpr / 25) + "%"), })
-    containers[2].css({"bottom": ((ypr/15) + "%"), "right": ((xpr / 15) + "%"), })
+    containers[2].css({"bottom": ((ypr/15) + "%"), "right": ((xpr / 15) + "%")})
+    containers[0].css({"bottom": ((ypr/30) + "%"), "right": ((xpr / 30) + "%")})
+    containers[1].css({"bottom": ((ypr/25) + "%"), "right": ((xpr / 25) + "%")})
 }).mouseover()
+
+//parallax mobile
+$(window).on("deviceorientation", (e) => {
+    let absolute = e.absolute,
+        x = Math.round(e.beta),
+        y = Math.round(e.gamma),
+        z = Math.round(e.alpha);
+
+    $("#debug").text(`Absolute: ${absolute}\nX:${x} Y:${y}; Z:${z}`)
+
+    containers[2].css({"bottom": ((y/15) + "%"), "right": ((x / 15) + "%")})
+    containers[0].css({"bottom": ((y/30) + "%"), "right": ((x / 30) + "%")})
+    containers[1].css({"bottom": ((y/25) + "%"), "right": ((x / 25) + "%")})
+})
